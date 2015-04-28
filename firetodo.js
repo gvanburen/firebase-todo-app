66ac2a3c3e871885666f64ebb010879feeb07b8d
Greg Van Buren <g.vanburen@gmail.com>
2015-04-27 20:21:31 -0700

    loggin in

--- firetodo.js
+++ firetodo.js
@@ -1,11 +1,20 @@
 angular.module('todoApp', ['firebase'])
-	.constant('FIREBASE_URL', 'https://burning-heat-343.firebaseio.com/')
+	.constant('FIREBASE_URL', 'https://burning-heat-343.firebaseio.com/todos')
 	.factory('firebaseReference', function(FIREBASE_URL){
 		return new Firebase( FIREBASE_URL );
 	})
-	.controller('toDoCtrl', ['$scope', '$firebaseArray', 'firebaseReference', function($scope, $firebaseArray, firebaseReference){
-		// var ref = new Firebase('https://burning-heat-343.firebaseio.com/')
-		$scope.items = $firebaseArray(firebaseReference.child('todos'));
+	.controller('toDoCtrl', ['$scope','$firebaseArray', 'firebaseReference', function($scope, $firebaseArray, firebaseReference){
+		$scope.logMeIn = function(){
+			var ref = firebaseReference;
+			ref.authWithOAuthPopup("google", function(error, authData) {
+			  if (error) {
+			    console.log("Login Failed!", error);
+			  } else {
+			    console.log("Authenticated successfully with payload:", authData);
+			    $scope.items = $firebaseArray(firebaseReference);
+			  }
+			})
+		}
 
 		$scope.addTodo = function(e){
 			if (e.keyCode != 13) return;
